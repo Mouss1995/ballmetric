054d1c4c2835cea91f08715a511b406d2e1e72e0
cuzonalan@gmail.com <cuzonalan@gmail.com>
2023-12-18 19:42:56 +0100

    Improved code: a single function for winter and summer leagues

--- generate_matchs_urls.py
+++ generate_matchs_urls.py
@@ -8,6 +8,7 @@ of each competition will be created, containing :
     - as well as a matches folder, which will contain the json files 
     for each scraped match.
 """
+
 import datetime
 import os
 import random
@@ -19,130 +20,80 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def initialization_summer_url_links():
+def link_initialization(type_league: str, df_league: pd.DataFrame) -> None:
     """
-    This function will retrieve the match links for each summer competition.
+    Function in order to generate links for each seasons
+    and create folder and csv files.
     """
+    df_league = df_league[df_league["type"] == type_league]
+    df_league = df_league.sample(frac=1)
 
-    df_competitions = pd.read_csv("competitions/init_competitions.csv")
-    df_competitions = df_competitions[df_competitions["type"] == "summer"]
-
-    for index, row in df_competitions.iterrows():
+    for _, row in df_league.iterrows():
         competition_name = row["name"]
-        print(f"#-------- Link initialization for {competition_name} --------#")
-
-        # Create folder if not exist
         if not os.path.exists("data/" + competition_name):
-            # Generate links for seasons and add to list
-            links = []
-            for i in range(2000, (datetime.datetime.now().year) + 1):
-                links.append(
+            print(f"#-------- Link initialization for {competition_name} --------#")
+            start_year = 2000
+            end_year = datetime.datetime.now().year
+            if type_league == "summer":
+                type_regex = r"(\d{4})"
+                links = [
                     row["part1"] + str(i) + row["part2"] + str(i) + row["part3"]
-                )
+                    for i in range(start_year, end_year + 1)
+                ]
+            if type_league == "winter":
+                type_regex = r"(\d{4}-\d{4})"
+                links = [
+                    f"{row['part1']}{i}-{i+1}{row['part2']}{i}-{i+1}{row['part3']}"
+                    for i in range(start_year, end_year + 1)
+                ]
 
             # Create a dataframe with all matchs links for each competition
             df_final = pd.DataFrame(columns=["Season", "Link"])
             for link in links:
                 urls = []
-                response = requests.get(link, timeout=10)
-                time.sleep(random.randrange(7, 15, 1))
-                soup = BeautifulSoup(response.content, "html.parser")
-                season_element = soup.select("h1")[0].get_text()
-                season_element = re.search(r"(\d{4})", season_element)
-                if season_element:
-                    season_element = season_element.group(1)
-                season_match = re.search(r"/(\d{4})/", link)
-                if season_match:
-                    season_match = season_match.group(1)
-                if season_element == season_match:
-                    elements_url = soup.select(".center a")
-                    for element in elements_url:
-                        href_value = element.get("href")
-                        if href_value:
-                            urls.append("https://fbref.com" + href_value)
-                if not urls:
-                    print(f"\t❌ No matchs for {season_match} season ❌")
-                    continue
+                while True:
+                    try:
+                        response = requests.get(link, timeout=10)
+                        time.sleep(random.randrange(7, 15, 1))
+                        soup = BeautifulSoup(response.content, "html.parser")
+                        season_element = soup.select("h1")[0].text
+                        season_element = re.search(type_regex, season_element)
+                        if season_element:
+                            season_element = season_element.group(1)
+                        season_match = re.search(r"/" + type_regex + "/", link)
+                        if season_match:
+                            season_match = season_match.group(1)
+                        if season_element == season_match:
+                            elements_url = soup.select(".center a")
+                            for element in elements_url:
+                                href_value = element.get("href")
+                                if href_value:
+                                    urls.append("https://fbref.com" + href_value)
+                        if not urls:
+                            print(
+                                f"\t\u274C No matches for the {season_match} season \u274C"
+                            )
+                            break
 
-                df_season = pd.DataFrame({"Season": season_match, "Link": urls})
-                df_season = df_season.drop_duplicates(subset=["Link"], keep="last")
-                df_season.reset_index(drop=True, inplace=True)
-                df_final = pd.concat([df_final, df_season])
-                print(f"\t✅ {len(df_season)} match links for {season_match} season ✅")
+                        df_season = pd.DataFrame({"Season": season_match, "Link": urls})
+                        df_season = df_season.drop_duplicates(
+                            subset=["Link"], keep="last"
+                        )
+                        df_season.reset_index(drop=True, inplace=True)
+                        df_final = pd.concat([df_final, df_season])
+                        print(
+                            f"\t\u2705 {len(df_season)} match links "
+                            f"for {season_match} season \u2705"
+                        )
+                        break
 
-            # Create directory and save dataframe
-            os.makedirs("data/" + competition_name)
-            os.makedirs("data/" + competition_name + "/matchs")
-            print(f"\t🗂️ Folder {competition_name} created")
-            df_final = df_final.drop_duplicates(subset=["Link"], keep="last")
-            df_final.reset_index(drop=True, inplace=True)
-            df_final.to_csv("data/" + competition_name + "/match_urls.csv", index=False)
-            print(f"\t=======> {len(df_final)} match links retrieved\n")
-
-
-def initialization_winter_url_links():
-    """
-    This function will retrieve the match links for each winter competition.
-    """
-
-    df_competitions = pd.read_csv("competitions/init_competitions.csv")
-    df_competitions = df_competitions[df_competitions["type"] == "winter"]
-
-    for index, row in df_competitions.iterrows():
-        competition_name = row["name"]
-        print(f"#-------- Link initialization for {competition_name} --------#")
-
-        # Create folder if not exist
-        if not os.path.exists("data/" + competition_name):
-            # Generate links for seasons and add to list
-            links = []
-            for i in range(2000, (datetime.datetime.now().year) + 1):
-                links.append(
-                    row["part1"]
-                    + str(i)
-                    + "-"
-                    + str(i + 1)
-                    + row["part2"]
-                    + str(i)
-                    + "-"
-                    + str(i + 1)
-                    + row["part3"]
-                )
-
-            # Create a dataframe with all matchs links for each competition
-            df_final = pd.DataFrame(columns=["Season", "Link"])
-            for link in links:
-                urls = []
-                response = requests.get(link, timeout=10)
-                time.sleep(random.randrange(7, 15, 1))
-                soup = BeautifulSoup(response.content, "html.parser")
-                season_element = soup.select("h1")[0].text
-                season_element = re.search(r"(\d{4}-\d{4})", season_element)
-                if season_element:
-                    season_element = season_element.group(1)
-                season_match = re.search(r"/(\d{4}-\d{4})/", link)
-                if season_match:
-                    season_match = season_match.group(1)
-                if season_element == season_match:
-                    elements_url = soup.select(".center a")
-                    for element in elements_url:
-                        href_value = element.get("href")
-                        if href_value:
-                            urls.append("https://fbref.com" + href_value)
-                if not urls:
-                    print(f"\t❌ No matchs for {season_match} season ❌")
-                    continue
-
-                df_season = pd.DataFrame({"Season": season_match, "Link": urls})
-                df_season = df_season.drop_duplicates(subset=["Link"], keep="last")
-                df_season.reset_index(drop=True, inplace=True)
-                df_final = pd.concat([df_final, df_season])
-                print(f"\t✅ {len(df_season)} match links for {season_match} season ✅")
+                    except requests.exceptions.Timeout:
+                        print(f"Timeout occurred {link}")
 
             # Create directory and save dataframe
             os.makedirs("data/" + competition_name)
             os.makedirs("data/" + competition_name + "/matchs")
-            print(f"\t🗂️ Folder {competition_name} created")
+            print(f"\t\U0001F5C2 Folder {competition_name} created")
             df_final = df_final.drop_duplicates(subset=["Link"], keep="last")
             df_final.reset_index(drop=True, inplace=True)
             df_final.to_csv("data/" + competition_name + "/match_urls.csv", index=False)
@@ -150,5 +101,8 @@ def initialization_winter_url_links():
 
 
 if __name__ == "__main__":
-    initialization_winter_url_links()
-    initialization_summer_url_links()
+    league = ["winter", "summer"]
+    df_competitions = pd.read_csv("competitions/init_competitions.csv")
+
+    for el in league:
+        link_initialization(el, df_competitions)
